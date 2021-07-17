65b93cb69599420d580c8f10c92d68faa941448e
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2021-07-17 08:16:56 +0530

    simios

---
+++ src/sistemamonitos/Mono.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemamonitos;
+//versionamiento con git
+// programcion de java
+
+
+/**
+ *
+ * @author ubuntu
+ */
+public class Mono {
+    
+    Mono(){
+    System.out.println("aqui en el constructor");
+    }
+    
+}
---
+++ src/sistemamonitos/Sistemamonitos.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemamonitos;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class Sistemamonitos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // Mono a;
+        //instancia
+         System.out.print("antes del constructor");
+       // a = new Mono();
+          System.out.print("despues del constructor");
+       
+    }
+    
+}
